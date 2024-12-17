3d786b25e218df5245d5ffd1ae690ae9fb7be7dd
Apaetus <katharina00000@gmail.com>
2024-12-17 10:23:08 +0300

    fix

--- accordion.js
+++ accordion.js
@@ -3,5 +3,6 @@ const accordionBtn = document.querySelector('#accordionBtn');
 for (i = 0; i < accordionBtn.length; i++) {
     accordionBtn[i].addEventListener('click', function () {
         this.classList.toggle('active');
+        console.log('кнопка нажата');
     });
 }
