5be296f3b5df684b31efa9971b28dbdba10a6cf5
Apaetus <katharina00000@gmail.com>
2024-12-18 19:07:47 +0300

    add: shine

---
+++ index.js
@@ -0,0 +1,23 @@
+const stopperConsultation = document.getElementById(
+    'stopper-consultation'
+);
+const consultationButton = document.getElementById('consultation');
+const options = {
+    root: null,
+    threshold: 0,
+};
+
+const callBack = (entries) => {
+    const isStop = entries[0].isIntersecting;
+    if (isStop) {
+        consultationButton.style = 'display: none';
+    } else {
+        consultationButton.style = '';
+    }
+
+    console.log(isStop);
+};
+
+const observer = new IntersectionObserver(callBack, options);
+
+observer.observe(stopperConsultation);
