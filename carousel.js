0bbf259e9c98a48cbe10a7c54244c5ff67fcae9f
Apaetus <katharina00000@gmail.com>
2025-04-02 19:06:46 +0300

    add: media for first block

--- carousel.js
+++ carousel.js
@@ -1,32 +1,37 @@
-const carouselListNode = document.querySelector('.carousel-list');
-const carouselItemNode = document.querySelectorAll('.carousel-item');
-const prevButtonNode = document.querySelector('.prev-btn');
-const nextButtonNode = document.querySelector('.next-btn');
-const buttonsNode = document.querySelector('.carousel-buttons');
+export function initCarousel(carouselContainer) {
+    const carouselListNode =
+        carouselContainer.querySelector('.carousel-list');
+    const carouselItemNode =
+        carouselContainer.querySelectorAll('.carousel-item');
+    const prevButtonNode =
+        carouselContainer.querySelector('.prev-btn');
+    const nextButtonNode =
+        carouselContainer.querySelector('.next-btn');
 
-let currentItem = 0;
-const totalItems = 4;
+    let currentItem = 0;
+    const totalItems = 4;
 
-function updateItemPosition() {
-    carouselListNode.style.transform = `translateX(-${
-        currentItem * 34
-    }%)`;
+    function updateItemPosition() {
+        carouselListNode.style.transform = `translateX(-${
+            currentItem * 34
+        }%)`;
+    }
+
+    prevButtonNode.addEventListener('click', () => {
+        if (currentItem > 0) {
+            currentItem--;
+        } else {
+            currentItem = totalItems - 1;
+        }
+        updateItemPosition();
+    });
+
+    nextButtonNode.addEventListener('click', () => {
+        if (currentItem < totalItems - 1) {
+            currentItem++;
+        } else {
+            currentItem = 0;
+        }
+        updateItemPosition();
+    });
 }
-
-prevButtonNode.addEventListener('click', (event) => {
-    if (currentItem > 0) {
-        currentItem--;
-    } else {
-        currentItem = totalItems - 1;
-    }
-    updateItemPosition();
-});
-
-nextButtonNode.addEventListener('click', () => {
-    if (currentItem < totalItems - 1) {
-        currentItem++;
-    } else {
-        currentItem = 0;
-    }
-    updateItemPosition();
-});
