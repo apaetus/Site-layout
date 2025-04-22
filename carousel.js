bb26dba92a1f0419320d34afed7ca5e9dabc6b14
Apaetus <katharina00000@gmail.com>
2025-04-22 12:05:40 +0300

    test

--- carousel.js
+++ carousel.js
@@ -14,17 +14,61 @@ export function initCarousel(carouselContainer) {
     let shiftSlide = slideWidth + GAP;
 
     let currentItem = 0;
-    const totalItems = carouselItemsNode.length;
+    let totalItems = carouselItemsNode.length;
+    let activeSlidesAmount = 3;
+
+    const firstItem = carouselItemsNode[0];
+    const secondItem = carouselItemsNode[1];
+    const secondLastItem =
+        carouselItemsNode[carouselItemsNode.length - 2];
+    const lastItem = carouselItemsNode[carouselItemsNode.length - 1];
+
+    firstItem.before(secondItem.cloneNode(true));
+    firstItem.before(secondLastItem.cloneNode(true));
+    firstItem.before(lastItem.cloneNode(true));
+    lastItem.after(secondLastItem.cloneNode(true));
+    lastItem.after(secondItem.cloneNode(true));
+    lastItem.after(firstItem.cloneNode(true));
 
     activateSlides();
 
+    function moveSlides(direction) {
+        if (direction === 'left') {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+        carouselListNode.classList.add('active');
+
+        if (currentItem < 0 || currentItem > totalItems - 1) {
+            carouselListNode.style.transform = `translate(-${
+                shiftSlide * (currentItem + 1)
+            }px)`;
+            carouselListNode.classList.remove('active');
+            currentItem = currentItem < 0 ? totalItems : 0;
+        }
+
+        carouselListNode.style.transform = `translateX(-${
+            currentItem * shiftSlide
+        }px)`;
+
+        activateSlides();
+    }
+
+    prevButtonNode.addEventListener('click', () => {
+        moveSlides('left');
+    });
+
+    nextButtonNode.addEventListener('click', () => {
+        moveSlides('right');
+    });
+
     function activateSlides() {
-        const activeSlidesAmount = getActiveSlidesAmount();
+        activeSlidesAmount = getActiveSlidesAmount();
         const lastActiveSlide = currentItem + activeSlidesAmount - 1;
         for (let i = 0; i < totalItems; i++) {
-            if (i < currentItem || i > lastActiveSlide) {
-                carouselItemsNode[i].classList.remove('active');
-            } else {
+            carouselItemsNode[i].classList.remove('active');
+            if (i >= currentItem && i <= lastActiveSlide) {
                 carouselItemsNode[i].classList.add('active');
             }
         }
@@ -39,36 +83,10 @@ export function initCarousel(carouselContainer) {
         activateSlides();
     });
 
-    function updateItemPosition() {
-        carouselListNode.style.transform = `translateX(-${
-            currentItem * shiftSlide
-        }px)`;
-    }
-
-    prevButtonNode.addEventListener('click', () => {
-        if (currentItem > 0) {
-            currentItem--;
-        } else {
-            currentItem = totalItems - 1;
+    function getActiveSlidesAmount() {
+        if (document.documentElement.clientWidth > 1350) {
+            return 3;
         }
-        updateItemPosition();
-        activateSlides();
-    });
-
-    nextButtonNode.addEventListener('click', () => {
-        if (currentItem < totalItems - 1) {
-            currentItem++;
-        } else {
-            currentItem = 0;
-        }
-        updateItemPosition();
-        activateSlides();
-    });
-}
-
-function getActiveSlidesAmount() {
-    if (document.documentElement.clientWidth > 1350) {
-        return 3;
+        return 2;
     }
-    return 2;
 }
